b880772ebd4dcfc8b4be7ec13d39839ac77f527c
Parker Culbertson <culbertsonp@ufl.edu>
2022-03-04 17:58:40 -0500

    subtraction update

--- GitHubTest.java
+++ GitHubTest.java
@@ -4,10 +4,27 @@ public class GitHubTest
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter two numbers to add: ");
-        int num1 = input.nextInt();
-        int num2 = input.nextInt();
-        int result = num1 + num2;
-        System.out.println("The result is: " + result);
+        System.out.println("Enter two numbers to add (1) or subtract (2): ");
+        int choice = input.nextInt();
+        if (choice == 1)
+        {
+            System.out.println("Enter numbers: ");
+            int num1 = input.nextInt();
+            int num2 = input.nextInt();
+            int result = num1 + num2;
+            System.out.println("The result is: " + result);
+        }
+        else if (choice == 2)
+        {
+            System.out.println("Enter numbers: ");
+            int num1 = input.nextInt();
+            int num2 = input.nextInt();
+            int result = num1 - num2;
+            System.out.println("The result is: " + result);
+        }
+        else
+        {
+            System.out.println("Error");
+        }
     }
 }
